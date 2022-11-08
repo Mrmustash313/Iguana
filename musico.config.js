e850224657425dce3d5daeb3f2597a73a6773029
Mrmustash313 <80130741+Mrmustash313@users.noreply.github.com>
2022-11-08 15:27:39 -0600

    aded config

---
+++ musico.config.js
@@ -0,0 +1,13 @@
+/**
+ * @typedef {Object} Config
+ * @property {string} [botId] 
+ * @property {string} [botToken] 
+ * @property {string} [ownerId] 
+ * @note 
+ **/
+const config = {
+	botId: "1026228681795973171",
+	botToken: "MTAyNjIyODY4MTc5NTk3MzE3MQ.GKEJhH.zDuNHCVjSoX8s1MO4Lcspq37E6Y8H-1sFnuMB4",
+	ownerId: "636233924644700170",
+};
+module.exports = config;
